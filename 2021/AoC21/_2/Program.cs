f49d23131fd85f41c283828cd7186278ba37a293
Maurice <mauricegolverdingen@hotmail.com>
2021-12-02 07:37:00 +0100

    Day 1 and day 2

---
+++ 2021/AoC21/_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using _2;
+
+var instructions = File.ReadAllLines("input.txt");
+
+//Part 1
+var sub1 = new Submarine();
+sub1.Go(instructions);
+
+Console.WriteLine(sub1.X * sub1.Depth);
+
+//Part 2
+var sub2 = new Submarine(aimMode: true);
+sub2.Go(instructions);
+
+Console.WriteLine(sub2.X * sub2.Depth);
---
+++ 2021/AoC21/_2/Submarine.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace _2
+{
+    public class Submarine
+    {
+        public int X { get; set; }
+        public int Depth { get; set; }
+        public int Aim { get; set; }
+        private bool _aimMode;
+
+        public Submarine(int x = 0, int d = 0, bool aimMode = false)
+        {
+            (X, Depth) = (0, 0);
+            Aim = 0;
+            _aimMode = aimMode;
+        }
+
+        public void Go(params string[] instructions)
+        {
+            foreach (var instruction in instructions)
+            {
+                var instr = instruction.Split(' ', StringSplitOptions.TrimEntries);
+                var code = Convert.ToInt32(instr[1]);
+                switch (instr[0])
+                {
+                    case "forward":
+                        X += code;
+                        if(_aimMode) Depth += Aim * code;
+                        break;
+                    case "down":
+                        if (_aimMode) Aim += code;
+                        else Depth += code;
+                        break;
+                    case "up":
+                        if (_aimMode) Aim -= code;
+                        else Depth -= code;
+                        break;
+                }
+            }
+        }
+    }
+}
