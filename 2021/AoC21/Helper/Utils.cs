28d1a2b9712244fc52b8ae6a0c04ad5efef36867
Maurice <mauricegolverdingen@hotmail.com>
2021-12-02 07:37:35 +0100

    Added 2021

---
+++ 2021/AoC21/Helper/Utils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Helper
+{
+    public static class Utils
+    {
+        public static int[] ReadLinesAsInt(string file)
+            => File.ReadAllLines(file).Select(x => Convert.ToInt32(x)).ToArray();
+
+        //LINQ extension to zip multiple collections
+        public static IEnumerable<TRes> ZipN<T,TRes>(this IEnumerable<T> coll, Func<IEnumerable<T>,TRes> selector, params IEnumerable<T>[] collections)
+        {
+            var enumerators = collections.Select(x => x.GetEnumerator()).ToList();
+            enumerators.Add(coll.GetEnumerator());
+            while (enumerators.All(x => x.MoveNext()))
+                yield return selector(enumerators.Select(x => x.Current));
+        }
+    }
+}
+
---
+++ 2021/AoC21/_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using Helper;
+
+//Part 1
+var input = Utils.ReadLinesAsInt("input.txt");
+var part1 = input.Zip(input.Skip(1), (i1, i2) => i2 - i1).Count(x => x > 0);
+Console.WriteLine(part1); //1233
+
+//Part 2: linq doesnt support zip with multiple, so I created my own LINQ extension (see utils.cs)
+var sums = input.ZipN(i => i.Sum(), input.Skip(1), input.Skip(2)).ToArray();
+var part2 = sums.Zip(sums.Skip(1), (i1, i2) => i2 - i1).Count(x => x > 0);
+Console.WriteLine(part2); //1275
